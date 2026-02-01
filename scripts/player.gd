1ec58a1935b21d9574a80415bb6ed1dde9746433
lucasjanta <lucastyle2014@gmail.com>
2026-02-01 20:44:16 -0300

    player script

---
+++ scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_force = -400.0
+@export var gravity_force : float = 900.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity_force * delta
+
+	move_and_slide()
